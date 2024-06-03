392408e26fa3725b74a3a6671b572c2b60f29442
Sven Tajz <sventajz3@gmail.com>
2024-06-03 12:02:09 +0200

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -64,13 +64,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            Study{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            study about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
