b34d395a46cd5eb6ed2ff93610e668c93c20faff
Sven Tajz <91728211+Sventajz@users.noreply.github.com>
2024-06-13 19:51:33 +0200

    cleanup for layout.js

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,18 +1,9 @@
 "use client";
 
-import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import { useServerInsertedHTML } from "next/navigation";
 import SideBar from "../../components/Sidebar";
 import { MainStyle, DashboardWrapper } from "./styled";
-
-const GlobalStyle = createGlobalStyle`
-  /* Your global styles here */
-`;
-
-const theme = {
-  /* Your theme here */
-};
 
 export default function RootLayout({ children }) {
   const sheet = typeof window === "undefined" ? new ServerStyleSheet() : null;
