01e1d13844ae542a4ae210f40bf9f682b16d5507
Sven Tajz <91728211+Sventajz@users.noreply.github.com>
2024-06-14 09:38:49 +0200

    fixed css loading after markup on refresh (FOUC) for both layout.js files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,29 @@
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+import { useServerInsertedHTML } from "next/navigation";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Vacation tracking app",
-  description:
-    "App for bussinesses for tracking and approving employee vacations",
-};
-
 export default function RootLayout({ children }) {
+  const sheet = typeof window === "undefined" ? new ServerStyleSheet() : null;
+
+  useServerInsertedHTML(() => {
+    return sheet ? sheet.getStyleElement() : null;
+  });
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>{sheet ? sheet.getStyleElement() : null}</head>
+      <body className={inter.className}>
+        {sheet ? (
+          <StyleSheetManager sheet={sheet.instance}>
+            {children}
+          </StyleSheetManager>
+        ) : (
+          children
+        )}
+      </body>
     </html>
   );
 }
